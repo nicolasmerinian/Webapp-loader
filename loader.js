82a39157999e5653ad2f1fde052293fb28624051
Nicolas Merinian <nicolas.merinian@gmail.com>
2015-12-18 01:17:35 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -19,11 +19,11 @@
  * 		- fadeOut(int delay).
  * 
  * Usage:
- * 		var Loader = new Loader(); // Already in commonVariables.js file as a global variable.
- * 		Loader.show(); // equivalent to: Loader.show(0);
+ * 		var loader = new loader();
+ * 		loader.show(); // equivalent to: loader.show(0);
  * 
  * 		The loader implements jQuery chaining so that it can be used like the following:
- * 		Loader.showFor(1000).show(3000).fadeOut(6000);
+ * 		loader.showFor(1000).show(3000).fadeOut(6000);
  * 
  * 		Use setWindowResizeEnabled(true) (default) to have the loader located in the center of the page.
  * 		Use setWindowResizeEnabled(false), then setX(x) and setY(y) to manually set the loader's position.
